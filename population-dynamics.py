26269fccb9bc1b6be7523c11f4d4e2570445ede2
Kamilly-Carvalho <carvalho.kamilly@academico.ifpb.edu.br>
2024-08-16 17:27:06 -0300

    add 3d graph generator

--- population-dynamics.py
+++ population-dynamics.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import random as rd
 import time
+from mpl_toolkits.mplot3d import Axes3D
 time.time()
 
 wks_s = np.array([1.0, 1.96, 2.425, 3.63])           #% da população de monócitos ao longo da gravidez em relação a não grávida
@@ -22,11 +23,14 @@ wks_d = 0.03 * wks_IFN                               #estímulo de produção de
 wks_beta1 = beta + wks_eta * nu / delta
 wks_c1 = wks_c + wks_d * wks_eta / delta
 
+remediocount = [i * 0.001 for i in range(1001)]
+# print (remediocount)
+
 h = 0.01                                             #parâmetro de precisão para método numérico de Runge-Kutta
 h1 = h/2                                             #parâmetro intermediário de precisão para método numérico de Runge-Kutta
 t = np.arange(0, 10+h, h)                            #vetor de tempo, até 10 dias
 time = len(t)
-pregnancy = len(wks_s)
+pregnancy = len(remediocount)
 kutta = 4
 
 s = np.zeros((pregnancy,time,kutta))
@@ -66,15 +70,15 @@ dados_3 = np.zeros((pregnancy+9,time,kutta))
 dados_4 = np.zeros((pregnancy+9,time,kutta))
 dados_5 = np.zeros((pregnancy+9,time,kutta))
 
-s[:,0,0] = 250 * wks_s
+s[:,0,0] = 250 
 i[:,0,0] = 10
 v[:,0,0] = 165
-z[:,0,0] = 2000 * wks_z
+z[:,0,0] = 2000 
 
-s_1[:,0,0] = 250 * wks_s
+s_1[:,0,0] = 250 
 i_1[:,0,0] = 10
 v_1[:,0,0] = 165
-z_1[:,0,0] = 2000 * wks_z
+z_1[:,0,0] = 2000 
 
 dados_x = np.zeros((pregnancy+55,time,kutta))
 
@@ -95,23 +99,43 @@ def graph_generator_3(eixo_x,eixo_y,t,dados,dados2,nome):
     plt.savefig(name,format='png')
     return 0
 
-for w in range(1):
-    for n in range(time - 1):
-        if(n <= day*100):#100/133/200/300/400/500
-            remedio = 1
-        else:
-            remedio = 0
-        for r in range(kutta):
-            
-            ds[w,n,r] = wks_mu[w] - alpha * s[w,n,r] - a[2] * s[w,n,r] * v[w,n,r]
-            di[w,n,r] = a[2] * s[w,n,r] * v[w,n,r] - beta * i[w,n,r] - nu * i[w,n,r] * z[w,n,r]
-            dv[w,n,r] = remedio * k * i[w,n,r] - gamma * v[w,n,r] - a[2] * s[w,n,r] * v[w,n,r]
-            dz[w,n,r] = wks_eta[w] + wks_c[w] * i[w,n,r] + wks_d[w] * i[w,n,r] * z[w,n,r] - delta * z[w,n,r]
+def graph_3D_generator(time,data1,data2,nome2,nome):
+    fig = plt.figure()
+    # Adiciona um subplot 3D
+    ax = fig.add_subplot(111, projection='3d')
 
-            ds_1[w,n,r] = wks_mu[w] - alpha * s_1[w,n,r] - a[2] * s_1[w,n,r] * v_1[w,n,r]
-            di_1[w,n,r] = a[2] * s_1[w,n,r] * v_1[w,n,r] - beta * i_1[w,n,r] - nu * i_1[w,n,r] * z_1[w,n,r]
-            dv_1[w,n,r] = k * i_1[w,n,r] - gamma * v_1[w,n,r] - a[2] * s_1[w,n,r] * v_1[w,n,r]
-            dz_1[w,n,r] = wks_eta[w] + wks_c[w] * i_1[w,n,r] + wks_d[w] * i_1[w,n,r] * z_1[w,n,r] - delta * z_1[w,n,r]
+    x = time
+    y = data1
+    X, Y = np.meshgrid(x, y)
+    Z = data2 # Exemplo de dados
+
+    # Plota a superfície
+    ax.plot_surface(X, Y, Z.reshape(X.shape), cmap='coolwarm')
+    # Adiciona rótulos aos eixos
+    ax.set_xlabel('Tempo')
+    ax.set_ylabel('Antiviral')
+    ax.set_zlabel(nome2)
+
+    ax.view_init(elev=35, azim=35)
+
+    plt.grid(color = 'gray', linestyle = '-', linewidth = 0.15)
+    name= nome +'.png'
+    plt.savefig(name,format='png')
+    return 0
+
+for w in range(len(remediocount)):
+    # print(w) 
+    for n in range(time - 1):
+        for r in range(kutta):
+            ds[w,n,r] = wks_mu[0] - alpha * s[w,n,r] - a[2] * s[w,n,r] * v[w,n,r]
+            di[w,n,r] = a[2] * s[w,n,r] * v[w,n,r] - beta * i[w,n,r] - nu * i[w,n,r] * z[w,n,r]
+            dv[w,n,r] = remediocount[w] * k * i[w,n,r] - gamma * v[w,n,r] - a[2] * s[w,n,r] * v[w,n,r]
+            dz[w,n,r] = wks_eta[0] + wks_c[0] * i[w,n,r] + wks_d[0] * i[w,n,r] * z[w,n,r] - delta * z[w,n,r]
+
+            # ds_1[w,n,r] = wks_mu[0] - alpha * s_1[w,n,r] - a[2] * s_1[w,n,r] * v_1[w,n,r]
+            # di_1[w,n,r] = a[2] * s_1[w,n,r] * v_1[w,n,r] - beta * i_1[w,n,r] - nu * i_1[w,n,r] * z_1[w,n,r]
+            # dv_1[w,n,r] = k * i_1[w,n,r] - gamma * v_1[w,n,r] - a[2] * s_1[w,n,r] * v_1[w,n,r]
+            # dz_1[w,n,r] = wks_eta[0] + wks_c[0] * i_1[w,n,r] + wks_d[0] * i_1[w,n,r] * z_1[w,n,r] - delta * z_1[w,n,r]
 
             if r < (kutta - 1):
                 s[w,n,r+1] = s[w,n,r] + h1 * ds[w,n,r]
@@ -119,32 +143,37 @@ for w in range(1):
                 v[w,n,r+1] = v[w,n,r] + h1 * dv[w,n,r]
                 z[w,n,r+1] = z[w,n,r] + h1 * dz[w,n,r]
 
-                s_1[w,n,r+1] = s_1[w,n,r] + h1 * ds_1[w,n,r]
-                i_1[w,n,r+1] = i_1[w,n,r] + h1 * di_1[w,n,r]
-                v_1[w,n,r+1] = v_1[w,n,r] + h1 * dv_1[w,n,r]
-                z_1[w,n,r+1] = z_1[w,n,r] + h1 * dz_1[w,n,r]
+                # s_1[w,n,r+1] = s_1[w,n,r] + h1 * ds_1[w,n,r]
+                # i_1[w,n,r+1] = i_1[w,n,r] + h1 * di_1[w,n,r]
+                # v_1[w,n,r+1] = v_1[w,n,r] + h1 * dv_1[w,n,r]
+                # z_1[w,n,r+1] = z_1[w,n,r] + h1 * dz_1[w,n,r]
 
         incremento_s[w,n] = (h/6) * (ds[w,n,0] + 2 * ds[w,n,1] + 2 * ds[w,n,2] + ds[w,n,3])
         incremento_i[w,n] = (h/6) * (di[w,n,0] + 2 * di[w,n,1] + 2 * di[w,n,2] + di[w,n,3])
         incremento_v[w,n] = (h/6) * (dv[w,n,0] + 2 * dv[w,n,1] + 2 * dv[w,n,2] + dv[w,n,3])
         incremento_z[w,n] = (h/6) * (dz[w,n,0] + 2 * dz[w,n,1] + 2 * dz[w,n,2] + dz[w,n,3])
 
-        incremento_s_1[w,n] = (h/6) * (ds_1[w,n,0] + 2 * ds_1[w,n,1] + 2 * ds_1[w,n,2] + ds_1[w,n,3])
-        incremento_i_1[w,n] = (h/6) * (di_1[w,n,0] + 2 * di_1[w,n,1] + 2 * di_1[w,n,2] + di_1[w,n,3])
-        incremento_v_1[w,n] = (h/6) * (dv_1[w,n,0] + 2 * dv_1[w,n,1] + 2 * dv_1[w,n,2] + dv_1[w,n,3])
-        incremento_z_1[w,n] = (h/6) * (dz_1[w,n,0] + 2 * dz_1[w,n,1] + 2 * dz_1[w,n,2] + dz_1[w,n,3])
+        # incremento_s_1[w,n] = (h/6) * (ds_1[w,n,0] + 2 * ds_1[w,n,1] + 2 * ds_1[w,n,2] + ds_1[w,n,3])
+        # incremento_i_1[w,n] = (h/6) * (di_1[w,n,0] + 2 * di_1[w,n,1] + 2 * di_1[w,n,2] + di_1[w,n,3])
+        # incremento_v_1[w,n] = (h/6) * (dv_1[w,n,0] + 2 * dv_1[w,n,1] + 2 * dv_1[w,n,2] + dv_1[w,n,3])
+        # incremento_z_1[w,n] = (h/6) * (dz_1[w,n,0] + 2 * dz_1[w,n,1] + 2 * dz_1[w,n,2] + dz_1[w,n,3])
 
         s[w,n+1,0] = s[w,n,0] + incremento_s[w,n]
         i[w,n+1,0] = i[w,n,0] + incremento_i[w,n]
         v[w,n+1,0] = v[w,n,0] + incremento_v[w,n]
         z[w,n+1,0] = z[w,n,0] + incremento_z[w,n]
         
-        s_1[w,n+1,0] = s_1[w,n,0] + incremento_s_1[w,n]
-        i_1[w,n+1,0] = i_1[w,n,0] + incremento_i_1[w,n]
-        v_1[w,n+1,0] = v_1[w,n,0] + incremento_v_1[w,n]
-        z_1[w,n+1,0] = z_1[w,n,0] + incremento_z_1[w,n]
+        # s_1[w,n+1,0] = s_1[w,n,0] + incremento_s_1[w,n]
+        # i_1[w,n+1,0] = i_1[w,n,0] + incremento_i_1[w,n]
+        # v_1[w,n+1,0] = v_1[w,n,0] + incremento_v_1[w,n]
+        # z_1[w,n+1,0] = z_1[w,n,0] + incremento_z_1[w,n]
 
-graph_generator_3('Tempo (dias)',u'Monócitos/\u03bcL',t,s,s_1,'Susceptible_monocyte_population')
-graph_generator_3('Tempo (dias)',u'Monócitos/\u03bcL',t,i,i_1,'Population_of_infected_monocytes')
-graph_generator_3('Tempo (dias)',u'Partículas virais/\u03bcL',t,v,v_1,'Zika_virus_population')
-graph_generator_3('Tempo (dias)',u'Linfócitos T/\u03bcL',t,z,z_1,'T-lymphocyte_population')
+# graph_generator_3('Tempo (dias)',u'Monócitos/\u03bcL',t,s,s_1,'Susceptible_monocyte_population')
+# graph_generator_3('Tempo (dias)',u'Monócitos/\u03bcL',t,i,i_1,'Population_of_infected_monocytes')
+# graph_generator_3('Tempo (dias)',u'Partículas virais/\u03bcL',t,v,v_1,'Zika_virus_population')
+# graph_generator_3('Tempo (dias)',u'Linfócitos T/\u03bcL',t,z,z_1,'T-lymphocyte_population')
+
+graph_3D_generator(t,remediocount,s[:, :, 0],'Monócitos sucetíveis','gráfico-s')
+graph_3D_generator(t,remediocount,i[:, :, 0],'Monócitos infectados','gráfico-i')
+graph_3D_generator(t,remediocount,v[:, :, 0],'Partículas virais','gráfico-v')
+graph_3D_generator(t,remediocount,z[:, :, 0],'Linfócitos T','gráfico-z')
